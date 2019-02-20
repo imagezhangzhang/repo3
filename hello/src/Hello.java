bd19af0a2983aa4b32769d7edde121f2c4090b80
dara <457719317@qq.com>
2019-02-20 10:32:54 +0800

    提交

---
+++ hello/src/Hello.java
@@ -0,0 +1,8 @@
+import javax.sound.midi.VoiceStatus;
+import java.io.PrintWriter;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+    }
+}
